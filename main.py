76441ce6d9f412a0fd49c92c0d84cb6cec9d10a4
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-02-10 02:05:32 +0100

    prise en charge des heurs mtn aussi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import re
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 
 id_canal = "1084908479745114212"
 url_envoi_message = f"https://discord.com/api/v9/channels/{id_canal}/messages"
@@ -20,12 +20,25 @@ donnees = {
     "tts": False
 }
 
+def extraire_temps(texte):
+    heures = 0
+    minutes = 0
+
+    match_heures = re.search(r"(\d+)h", texte.replace("**", ""))
+    match_minutes = re.search(r"(\d+) min", texte.replace("**", ""))
+
+    if match_heures:
+        heures = int(match_heures.group(1))
+    if match_minutes:
+        minutes = int(match_minutes.group(1))
+
+    return (heures * 60) + minutes  
+
 def envoyer_message():
     reponse_envoi = requests.post(url_envoi_message, headers=entetes, data=json.dumps(donnees))
 
     if reponse_envoi.status_code == 200:
         print("\033[92m[SUCCESS] Message envoyé avec succès !\033[0m")
-
 
         reponse_recup = requests.get(url_recup_message, headers=entetes)
         
@@ -37,19 +50,18 @@ def envoyer_message():
             print(f"Envoyeur : {id_envoyeur}")
             
             if id_envoyeur == "432610292342587392":
-                correspondance = re.search(r"(\d+) min", dernier_message['content'].replace("**", ""))
-                if correspondance:
-                    temps_restant = int(correspondance.group(1))
+                temps_restant = extraire_temps(dernier_message['content'])
+                if temps_restant > 0:
                     print(f"Temps restant avant le prochain message: {temps_restant} minutes")
                     attendre_temps_restant(temps_restant)
                 else:
-                    print("Je trouve pas le bon temps zbi")
+                    print("\033[91m[ERROR] Temps restant incorrect ou non trouvé.\033[0m")
             else:
-                print(f"Qlq d'autres à écrit?")
+                print(f"L'envoyeur n'est pas correct (ID: {id_envoyeur}).")
         else:
-            print(f"\033[91mERROR : {reponse_recup.status_code}\033[0m")
+            print(f"\033[91m[ERROR] Erreur lors de la récupération du message : {reponse_recup.status_code}\033[0m")
     else:
-        print(f"\033[91mERROR : {reponse_recup.status_code}\033[0m")
+        print(f"\033[91m[ERROR] Erreur lors de l'envoi du message : {reponse_envoi.status_code}\033[0m")
         print(reponse_envoi.text)
 
 def attendre_temps_restant(temps_restant):
